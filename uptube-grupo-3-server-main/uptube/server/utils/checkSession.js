a087fe95289bd364ebae7e541f384112897a1ee1
CabralFM <105600801+CabralFM@users.noreply.github.com>
2023-01-25 15:00:05 +0000

    Delete uptube-grupo-3-server-main directory

--- uptube-grupo-3-server-main/uptube/server/utils/checkSession.js
+++
@@ -1,8 +0,0 @@
-const checkSession = (req, res, next) => {
-    if(req.session.id_user){
-        return next();
-    }
-    res.status(401).json({success: false, error: "not_authenticated"})
-}
-
-module.exports = {checkSession};
